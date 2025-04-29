f850dc9164b1b2d7ad7eaa9ef2db941d0b80f2bd
leonmsindwana <lennymsindwana@gmail.com>
2025-04-29 17:00:56 +0200

    my intro to programming files

---
+++ introduction/second book/chapter twelve/chap12.js
@@ -0,0 +1,50 @@
+var trainSpeed = 250;
+var trainPosition = 0;
+var animation;
+
+var train = document.getElementById("train");
+train.addEventListener("click", speedUp);
+
+var stopButton = document.getElementById("stopButton");
+stopButton.addEventListener("click", stopTrain);
+
+function speedUp() {
+    if (trainSpeed > 10) {
+        trainSpeed -= 10;
+    }
+    console.log("train speed: " + trainSpeed);
+
+    clearInterval(animation);
+    animation = setInterval(frame, trainSpeed);
+
+
+    function frame() {
+        trainPosition += 2;
+        train.style.left = trainPosition + 'px';
+        console.log(trainPosition);
+        checkPosition(trainPosition);
+    }
+}
+
+function checkPosition(currentPosition) {
+    if (currentPosition === 400) {
+        alert("OOOOO!");
+        console.log("Crash!");
+        clearInterval(animation);
+    }
+}
+
+function stopTrain() {
+    if (trainPosition < 260) {
+        clearInterval(animation);
+    }
+}
+function endOfTrack(){
+if (currentPosition >= 400) {
+    trainPosition = 0;
+    train.style.left = trainPosition + 'px';
+    console.log("Train reset to 0");
+}
+
+
+}
