715dfeb1cd00dc33d7f0fb39c5a5e1ffe3e087a8
leonmsindwana <lennymsindwana@gmail.com>
2025-07-24 17:39:14 +0200

    chap

---
+++ second book/chapter eight/car.js
@@ -0,0 +1,25 @@
+var dreamCar = {
+    make: "Oldsmobile",
+    model: "98",
+    color: "brown",
+    year: 1983,
+    bodyStyle: "Luxury Car",
+    price: 4500
+};
+
+alert("The type of dreamCar is: " + typeof dreamCar);
+
+document.getElementById("pricetag").innerHTML = dreamCar.price;
+document.getElementById("modelyear").innerHTML = dreamCar.year;
+document.getElementById("body").style.backgroundColor = dreamCar.color;
+
+var body = document.getElementById("body");
+
+body.style.borderRadius = "25px";
+
+body.style.backgroundColor = "blue";
+
+var backWheel = document.getElementById("backwheel");
+  
+backWheel.style.backgroundColor = "red";
+
